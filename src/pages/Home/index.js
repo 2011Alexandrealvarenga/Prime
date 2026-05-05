fbb0acea846acf22f4daf501a06d48bf3743f401
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2026-05-05 19:24:25 -0300

    posts - se demorar para carregar o post entao faça um aviso de carregando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,12 @@
 import {useEffect, useState} from 'react';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
+import './home.css';
 
 function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(()=>{
         async function loadFilmes(){
             const response = await api.get('movie/now_playing', {
@@ -13,15 +17,38 @@ function Home(){
                     page: 1,
                 }
             })
-            console.log(response.data);
+            setFilmes(response.data.results.slice(0,10));
+            setLoading(false);
 
         }
         loadFilmes();
     }, []); 
+
+    // se demorar para carregar, mostrar a mensagem de carregando
+
+    if(loading){
+        return(
+            <div className='loading'>
+                <p>carregando filmes ...</p>
+            </div>
+        )}
+
+
     return(
-        <div>
-            <h1>Home</h1>
-            <p>Bem-vindo à página inicial!</p>
+        <div className='container'>
+            <div className='lista-filmes'>
+                    {filmes.map((filme)=>{
+                        return(
+                            <article key={filme.id}>
+                                <strong>{filme.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })}
+                
+            </div>
+
         </div>
     );
 }
